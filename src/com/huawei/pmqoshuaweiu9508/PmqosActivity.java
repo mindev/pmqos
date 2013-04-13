ff12deea96e739ab6ab90b0c4473bec2b3ab5ff4
Marco Fassi <marco@Aspire-5742G.(none)>
2013-04-13 13:26:02 +0100

    inizio prima activity

--- src/com/huawei/pmqoshuaweiu9508/PmqosActivity.java
+++ src/com/huawei/pmqoshuaweiu9508/PmqosActivity.java
@@ -1,22 +1,75 @@
 package com.huawei.pmqoshuaweiu9508;
 
+import java.util.HashMap;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class PmqosActivity extends Activity {
+public class PmqosActivity extends Activity implements OnClickListener{
 
+	//# instance fields
+	private Spinner mApps;
+
+private Button mButton;
+
+private Spinner mCPUNumber;
+
+private EditText mCpuMax;
+
+private String mCpuNumber;
+
+private EditText mGpuMax;
+
+private HashMap mPolicy;
+.annotation system Ldalvik/annotation/Signature;
+    value = {
+        "Ljava/util/HashMap",
+        "<",
+        Integer",
+        "Integer;",
+        ">;"
+    }
+.end annotation
+.end field
+
+private String mProcessName;
+
+    //# direct methods
+    public constructor init(){ 
+    		<init>()V
+        .locals 1
+
+        .prologue
+        .line 26
+        invoke-direct {p0}, Landroid/app/Activity;-><init>()V
+
+        .line 29
+        		const/4 v0, 0x0
+
+        		iput-object v0, p0, Lcom/huawei/pmqos/PmqosActivity;->mApps:Landroid/widget/Spinner;
+
+        		return void
+        		}
+ 
+
+	//public interface android/view/View$OnClickListener;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
+
+
 
 }
